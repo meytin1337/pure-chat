acd3118cba6ec1649548e556c331f1389bfde601
Mats Heemeyer <heemeyer@ams-gruppe.de>
2026-04-15 09:46:24 +0200

    fix: make GEMINI.md optional

--- src/pure_chat/ai_manager.py
+++ src/pure_chat/ai_manager.py
@@ -1,9 +1,7 @@
 import os
 from google import genai
 from google.genai import types
-import tomllib
 from pure_chat.fs import config_path
-from pure_chat.util import select_model
 
 
 class GeminiAssistant:
@@ -20,8 +18,9 @@ class GeminiAssistant:
         self.client = genai.Client(api_key=api_key)
 
         instr = "You are a helpful assistant."
-        if config_path("GEMINI.md"):
-            with open("GEMINI.md", "r") as f:
+        instr_file = config_path("GEMINI.md")
+        if instr_file.exists():
+            with open(instr_file, "r") as f:
                 instr = f.read()
 
         # 2. Configure Tools and Token Caps
